072a2b50128ff38d5f452ccabe0e14e64cb21572
jirapornBiw <90706962+jirapornBiw@users.noreply.github.com>
2023-03-16 14:03:06 +0700

    .

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="resume">
+  <img src="277442717_3116177675378267_8707825760468727216_n.jpg">
+  <h1>Jaturaphat Churnchom</h1>
+  <p>Cell: <a href=#>0806712417</a>
+  <p>Email: <a href=#>jaturaphatpeme@gmail.com</a>
+  <p id="objective">I am a 4th year computer science student Rajamangala University of Technology Thanyaburi
+
+<dl>
+  <dt>Education
+  <dd>
+    <h2>Rajamangala University of Technology Thanyaburi</h2>
+    <p><strong>Major:</strong>computer science<br/>
+</dl>
+<dl>
+<dt>Skills
+<dd>
+<h2>Office Skills</h2>
+<p>Office and records management, database administration, event organization, customer support, travel coordination
+<h2>Computer skills</h2>
+<p>Microsoft productivity software (Word, Excel, etc), Adobe Creative Suite, Windows
+</dl>
+<dl>
+<dt>Experience
+<dd>
+<h2>-</h2>
+</dd>
+</dt>
+</dl>
+<dl>
+<dt>Hobbies
+<dd>World domination, Social Manipulation, Murder Most Foul
+</dl>
+<dl>
+<dt>References
+<dd>Available on request
+</dl>
+</div>
+</body>
+</html>
